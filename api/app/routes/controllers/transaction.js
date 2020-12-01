9ff5ec784cbee37073a2d8a6cee86c027be34a96
João Gabriel de Matos <joaogabriel.c.matos@gmail.com>
2020-12-01 19:20:31 -0300

    Correçao da criaçao de transaçoes

--- api/app/routes/controllers/transaction.js
+++ api/app/routes/controllers/transaction.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import httpStatus from 'http-status';
 import { user as UserEntity } from '../../models/index';
-import transaction from '../../models/index';
+import { transaction as Transaction} from '../../models/index';
 
 const routes = express.Router();
 
@@ -12,7 +12,8 @@ routes.post('/',
             // logics
             const check = await UserEntity.findOne({ where: { id: req.body.userId } })
             if (check) {
-                response = await transaction.create(req.body);
+                response = await Transaction.create(req.body);
+
                 return res.status(httpStatus.OK).json(response);
             } else {
                 return res.status(401).json({message:'Login e tente novamente'})
