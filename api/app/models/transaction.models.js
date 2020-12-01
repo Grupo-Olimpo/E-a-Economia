c1e5552a032d003b9145103a494b2758b02c3291
João Gabriel de Matos <joaogabriel.c.matos@gmail.com>
2020-12-01 15:34:24 -0300

    Criaçao da migration e da model de transaçao

---
+++ api/app/models/transaction.models.js
@@ -0,0 +1,17 @@
+module.exports = (sequelize, Sequelize) => {
+    const Transaction = sequelize.define("Transaction", {
+        name: { type: sequelize.STRING, field: 'str_name' },
+        description: { type: sequelize.STRING, field: 'str_description' },
+        date: { type: sequelize.STRING, field: 'str_date' }
+    }, {
+        tableName: 'tb_transaction',
+        freezeTableName: true,
+        timestamps: true,
+
+        createdAt: 'dt_created_at',
+        updatedAt: 'dt_updated_at',
+        deletedAt: 'dt_deleted_at',
+    });
+
+    return Transaction;
+}
---
+++ api/migrations/20201130211857-transaction.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('tb_transaction', {
+
+      id:{  type: Sequelize.BIGINT,
+            allowNull: false,
+            autoIncrement: true,
+          },
+
+      str_name: { type: Sequelize.STRING,
+                  allowNull: false
+                },
+      str_description: { type: Sequelize.STRING },
+      str_date: { type: Sequelize.STRING },
+
+      dt_created_at: { type: Sequelize.DATE },
+      dt_updated_at: { type: Sequelize.DATE },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+  
+    await queryInterface.dropTable('tb_transaction');
+
+    
+  }
+};
